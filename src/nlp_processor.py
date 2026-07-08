4fe443aa906e6b599fef08aa55efa79f7b6eaca0
Cozma Vlad <vladuzz208@gmail.com>
2026-07-08 17:10:58 +0300

    Optimize code

--- src/nlp_processor.py
+++ src/nlp_processor.py
@@ -4,27 +4,13 @@ from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
 from pathlib import Path
 
-
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
-TRAIN_DATA_PATH = PROCESSED_DATA_DIR / "train_data.csv"
-TEST_DATA_PATH = PROCESSED_DATA_DIR / "test_data.csv"
-VAL_DATA_PATH = PROCESSED_DATA_DIR / "val_data.csv"
-
 MOVIES_DATA_PATH = DATA_DIR / "movies.csv"
 
-
-def load_datasets():
-    train_df = pd.read_csv(TRAIN_DATA_PATH)
-    test_df = pd.read_csv(TEST_DATA_PATH)
-    val_df = pd.read_csv(VAL_DATA_PATH)
-    movies_df = pd.read_csv(MOVIES_DATA_PATH)
-
-    return movies_df, train_df, test_df, val_df
-
-def build_recommender(movies_df, train_df):
+def build_recommender(movies_df):
     cv=TfidfVectorizer()
     tfidf_matrix=cv.fit_transform(movies_df['genres'])
     cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
@@ -43,8 +29,8 @@ def recommendations(titles, indices, cosine_sim, title):
     return titles.iloc[movie_indices]
 
 def main():
-    movies_df, train_df, _, _ = load_datasets()
-    titles, indices, cosine_sim = build_recommender(movies_df, train_df)
+    movies_df = pd.read_csv(MOVIES_DATA_PATH)
+    titles, indices, cosine_sim = build_recommender(movies_df)
     print(
         recommendations(
             titles,
